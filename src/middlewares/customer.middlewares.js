c1095a1e3d83f6f849ffc22ff77980ab65b6a0ef
ana-lapas <anapaulalapas@gmail.com>
2023-02-10 19:48:30 -0400

    Fix: putCustomer

--- src/middlewares/customer.middlewares.js
+++ src/middlewares/customer.middlewares.js
@@ -26,3 +26,30 @@ export async function validateCustomer(req,res,next){
     res.locals.customer = customer;
     next();
 };
+ export async function updateValidation(req, res, next){
+    const customer = req.body;
+    const{ id }= req.params;    
+    
+    const { error } = customerSchema.validate(customer, { abortEarly: false });
+
+    if (error) {
+        const errors = error.details.map(detail => detail.message);
+        return res.status(400).send(errors);
+    }
+    
+    try {
+        const existingCustomer = await db.query(`SELECT * FROM customers WHERE id=$1`, [id]);
+        if (existingCustomer.rows.length === 0) {
+            return res.sendStatus(404);
+        }
+        const checkCPF = await db.query(`SELECT * FROM customers WHERE id=$1`, [id]);
+        if (checkCPF.rows[0].id != id) {
+            return res.sendStatus(409);
+        }
+    } catch (err) {
+        res.status(500).send(err.message);
+        return;
+    }
+    res.locals.customer = customer;
+    next();
+ }
--- src/routers/customer.routes.js
+++ src/routers/customer.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { getCustomers, getCustomerId, postCustomer, putCustomer } from "../controllers/customer.controllers.js";
-import { validateCustomer } from "../middlewares/customer.middlewares.js";
+import { validateCustomer, updateValidation } from "../middlewares/customer.middlewares.js";
 const customerRouter = Router();
 
 customerRouter.get("/customers", getCustomers);
 customerRouter.post("/customers", validateCustomer, postCustomer);
 customerRouter.get("/customers/:id", getCustomerId);
-customerRouter.put("/customers/:id", validateCustomer, putCustomer );
+customerRouter.put("/customers/:id", updateValidation, putCustomer );
 export default customerRouter;
