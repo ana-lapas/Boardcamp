06c60790f55601427ed9df12314eb33e6619da3d
ana-lapas <anapaulalapas@gmail.com>
2023-02-10 21:15:35 -0400

    Fix: putCustomer 3

--- src/controllers/customer.controllers.js
+++ src/controllers/customer.controllers.js
@@ -40,12 +40,10 @@ export async function postCustomer(req, res) {
 
 export async function putCustomer(req, res) {
     const { name, phone, cpf, birthday } = res.locals.customer;
-    const { id } = req.params;
-    try {
-        
+    const { id } = res.locals.id;
+    try {        
         await db.query('UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5', [name,phone,cpf,birthday,id]);
         return res.sendStatus(200);
-
     } catch (err) {
         return res.status(500).send(err.message);
     }
--- src/middlewares/customer.middlewares.js
+++ src/middlewares/customer.middlewares.js
@@ -2,17 +2,17 @@ import { db } from "../database/database.connection.js";
 import { customerSchema } from "../schemas/customer.schemas.js";
 import dayjs from "dayjs";
 
-export async function validateCustomer(req,res,next){
+export async function validateCustomer(req, res, next) {
     const customer = req.body;
-    const{ cpf }= req.body;    
-    
+    const { cpf } = req.body;
+
     const { error } = customerSchema.validate(customer, { abortEarly: false });
 
     if (error) {
         const errors = error.details.map(detail => detail.message);
         return res.status(400).send(errors);
     }
-    
+
     try {
         const existingCustomer = await db.query(`SELECT * FROM customers WHERE cpf=$1`, [cpf]);
         if (existingCustomer.rows.length > 0) {
@@ -26,31 +26,34 @@ export async function validateCustomer(req,res,next){
     res.locals.customer = customer;
     next();
 };
- export async function updateValidation(req, res, next){
+export async function updateValidation(req, res, next) {
+    const { id } = req.params;
+    const { cpf } = req.body;
     const customer = req.body;
-    const{ id }= req.params;   
-    const {cpf} = req.body; 
-    
     const { error } = customerSchema.validate(customer, { abortEarly: false });
 
     if (error) {
         const errors = error.details.map(detail => detail.message);
         return res.status(400).send(errors);
     }
-    
+
     try {
-        const existingCustomer = await db.query(`SELECT * FROM customers WHERE id=$1`, [id]);        
+        const existingCustomer = await db.query(`SELECT * FROM customers WHERE id=$1`, [id]);
         if (existingCustomer.rows.length === 0) {
             return res.sendStatus(404);
-        }        
-        const checkCPF = await db.query(`SELECT * FROM customers WHERE cpf=$1`, [cpf]);        
-        if (id != checkCPF.rows[0].id ) {
+        }
+
+        const checkCPF = await db.query(`SELECT * FROM customers WHERE id!=$1 AND cpf=$2`, [id, cpf]);
+        if (checkCPF.rows.length > 0) {
             return res.sendStatus(409);
         }
+
     } catch (err) {
         res.status(500).send(err.message);
         return;
     }
+
     res.locals.customer = customer;
-    next();
- }
+    res.locals.id = id;
+    next()
+}
